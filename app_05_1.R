d4e192d6dee1b653b4b63d220ca60bf225cca2c7
eternity618 <eternity618@gmail.com>
2022-05-20 13:00:06 +0900

    1

---
+++ app_05_1.R
@@ -0,0 +1,67 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(DT)
+install.packages("DT")
+
+players <- read.csv("data/nba2018.csv")
+
+ui <- fluidPage(
+  titlePanel("NBA 2018/19 Player Stats"),
+  sidebarLayout(
+    sidebarPanel(
+      "Exploring all player stats from the NBA 2018/19 season",
+      h3("Filters"),
+      sliderInput(
+        inputId = "VORP",
+        label = "Player VORP rating at least",
+        min = -3, max = 10,
+        value = 0
+      ),
+      selectInput(
+        "Team", "Team",
+        unique(players$Team),
+        selected = "Golden State Warriors"
+      )
+    ),
+    mainPanel(
+      strong(
+        "There are",
+        textOutput("num_players", inline = TRUE),
+        "players in the dataset"
+      ),
+      plotOutput("nba_plot"),
+      tableOutput("players_data")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  data <- players %>%
+    filter(VORP >= input$VORP,
+           Team %in% input$Team)}
+  
+  output$players_data <- renderDT({
+    
+    data()
+  })
+  
+  output$num_players <- renderText({
+    
+    nrow(data())
+  })
+  
+  
+  # Build the plot output here
+  output$nba_plot <- renderPlot({
+    data <- players %>%
+      filter(VORP >= input$VORP,
+             Team %in% input$Team)
+    
+    ggplot(data, aes(Salary)) +
+      geom_histogram()
+  })
+  
+}
+
+shinyApp(ui, server)
